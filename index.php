5b206ed5212de9af50bdbef939acb9830da3aa36
biloubed <45163267+biloubed@users.noreply.github.com>
2022-11-16 17:07:09 +0100

    aiie miguel 3.0
    zedz

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php
 
 echo("hello world");
-
+echo("AIIE MIGUELLLL");
 
 ?>
 
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <title>Titre de la page</title>
-</head>
-<body>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/H-L2RxUtdwc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</body>
-</html>
 
 
 
